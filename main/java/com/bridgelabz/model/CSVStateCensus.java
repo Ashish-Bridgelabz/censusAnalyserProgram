e44978694f810b77dd8badf496c2e216a6a75f12
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-07 00:19:51 +0530

    ADDED:POJO read directy CSV file

---
+++ main/java/com/bridgelabz/model/CSVStateCensus.java
@@ -0,0 +1,47 @@
+package com.bridgelabz.model;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStateCensus {
+    @CsvBindByName(column = "State")
+    public String State;
+    @CsvBindByName(column = "Population")
+    public String Population;
+    @CsvBindByName(column = "AreaInSqKm")
+    public String AreaInSqKm;
+    @CsvBindByName(column = "DensityPerSqKm")
+    public String DensityPerSqKm;
+
+    //Getter and setter method to encapsulate
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getPopulation() {
+        return Population;
+    }
+
+    public void setPopulation(String population) {
+        Population = population;
+    }
+
+    public String getAreaInSqKm() {
+        return AreaInSqKm;
+    }
+
+    public void setAreaInSqKm(String areaInSqKm) {
+        AreaInSqKm = areaInSqKm;
+    }
+
+    public String getDensityPerSqKm() {
+        return DensityPerSqKm;
+    }
+
+    public void setDensityPerSqKm(String densityPerSqKm) {
+        DensityPerSqKm = densityPerSqKm;
+    }
+}
