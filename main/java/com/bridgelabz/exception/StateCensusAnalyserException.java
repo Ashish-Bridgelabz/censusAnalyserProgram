4097db3ec50618195af51ad0e056a50a3f9751b8
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-07 19:06:46 +0530

    ADDED: ENTERED_INCORRECT_DELIMITER enum exception

--- main/java/com/bridgelabz/exception/StateCensusAnalyserException.java
+++ main/java/com/bridgelabz/exception/StateCensusAnalyserException.java
@@ -2,7 +2,7 @@ package com.bridgelabz.exception;
 
 public class StateCensusAnalyserException extends Exception {
     public enum Exceptiontype {
-        ENTERED_WRONG_FILE,ENTER_WRONG_FILE_TYPE;
+        ENTERED_WRONG_FILE,ENTER_WRONG_FILE_TYPE,ENTERED_INCORRECT_DELIMITER;
     }
 
     public Exceptiontype type;
