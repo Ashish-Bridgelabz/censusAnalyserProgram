fdfb62501ec4029126d3b459135e563c886d5707
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-07 23:23:06 +0530

    ADDED:Check all the record

--- test/java/com/bridgelabz/service/Test/StateCensusAnalyserTest.java
+++ test/java/com/bridgelabz/service/Test/StateCensusAnalyserTest.java
@@ -1,11 +1,11 @@
 package com.bridgelabz.service.Test;
 
 import com.bridgelabz.exception.StateCensusAnalyserException;
+import com.bridgelabz.service.StateCensus;
 import com.bridgelabz.service.StateCensusAnalyser;
 import org.junit.Assert;
 import org.junit.Test;
 
-//import java.io.File;
 import java.io.File;
 import java.io.IOException;
 
@@ -15,6 +15,8 @@ public class StateCensusAnalyserTest {
     String STATE_CSV_WRONG_FILE_TYPE_PATH = "/home/ashish/IdeaProjects/IndianCensusAnalyser/src/test/resources/IndiaStateCensusData.txt";
     String STATE_CSVFILE_DELIMITERINCORRECT_PATH = "/home/ashish/IdeaProjects/IndianCensusAnalyser/src/test/resources/IndiaStateCensusData1.csv";
     String STATE_CSVHEADER_INCORRECT_PATH = "/home/ashish/IdeaProjects/IndianCensusAnalyser/src/test/resources/IndiaStateCensusData2.csv";
+    //CSV State code file path
+    private final String CSV_STATE_CODE_PATH = "/home/ashish/IdeaProjects/IndianCensusAnalyser/src/test/resources/IndiaStateCode.csv";
     StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
 
     @Test
@@ -62,5 +64,11 @@ public class StateCensusAnalyserTest {
         } catch (StateCensusAnalyserException e) {
             Assert.assertEquals(StateCensusAnalyserException.Exceptiontype.ENTERED_INCORRECT_DELIMITER_OR_HEADER, e.type);
         }
+    }
+    @Test
+    public void givenStateCodeCsvFile_WhenTrue_NumberOfRecordShouldMatch() throws IOException, StateCensusAnalyserException {
+        StateCensus stateCensus = new StateCensus();
+        int totalRecords = stateCensus.loadCensusCodeCSVData(CSV_STATE_CODE_PATH);
+        Assert.assertEquals(37, totalRecords);
     }
 }
