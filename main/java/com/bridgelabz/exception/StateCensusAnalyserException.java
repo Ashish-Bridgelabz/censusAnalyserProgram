bf9e2843df536060f0967d66a23053578e92a131
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-07 17:43:49 +0530

    ADDED:ENTERED_WRONG_FILE_TYPE enum exception

--- main/java/com/bridgelabz/exception/StateCensusAnalyserException.java
+++ main/java/com/bridgelabz/exception/StateCensusAnalyserException.java
@@ -2,7 +2,7 @@ package com.bridgelabz.exception;
 
 public class StateCensusAnalyserException extends Exception {
     public enum Exceptiontype {
-        ENTERED_WRONG_FILE;
+        ENTERED_WRONG_FILE,ENTER_WRONG_FILE_TYPE;
     }
 
     public Exceptiontype type;
