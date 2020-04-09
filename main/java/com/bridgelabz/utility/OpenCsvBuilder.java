9d7784c5bf3cb477c7849ff5120bae3cf7aa1ecc
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-10 00:59:16 +0530

    ADDED:Change getCSVFileList into generic method

---
+++ main/java/com/bridgelabz/utility/OpenCsvBuilder.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.utility;
+
+import com.bridgelabz.exception.StateCensusAnalyserException;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+
+public class OpenCsvBuilder {
+    //Generic method to iterate through csv file
+    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws StateCensusAnalyserException {
+        try {
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
+            Iterator<E> censusCSVIterator = csvToBean.iterator();
+            return censusCSVIterator;
+        } catch (IllegalStateException e) {
+            throw new StateCensusAnalyserException
+                    (StateCensusAnalyserException.Exceptiontype.ENTERED_WRONG_FILE, e.getMessage());
+        }
+
+    }
+}
+
+
+
