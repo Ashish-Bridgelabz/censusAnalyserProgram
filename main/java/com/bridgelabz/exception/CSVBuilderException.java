84bcd8337de87bec2914c8e085f3f1054ac6e0a7
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-11 00:45:06 +0530

    ADDED:Handle exception by CSVBuilderException

--- main/java/com/bridgelabz/exception/CSVBuilderException.java
+++ main/java/com/bridgelabz/exception/CSVBuilderException.java
@@ -1,15 +1,16 @@
 package com.bridgelabz.exception;
 
-public class CSVBuilderException extends Exception{
-
+public class CSVBuilderException extends Exception {
     public enum Exceptiontype {
-        ENTERED_WRONG_FILE;
+        ENTERED_WRONG_FILE_TYPE, ENTERED_INCORRECT_DELIMITER, ENTERED_WRONG_FILE,
+        ENTERED_INCORRECT_DELIMITER_OR_HEADER, UNABLE_TO_PARSE, ILLEGAL_STATE;;
+
     }
+
     public Exceptiontype type;
 
-       public CSVBuilderException(Exceptiontype type, String message) {
+    public CSVBuilderException(Exceptiontype type, String message) {
         super(message);
         this.type = type;
     }
 }
-
