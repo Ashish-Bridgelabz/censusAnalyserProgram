953144185f838bc6554d98b6387b2dbb9349dfdd
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-11 11:50:29 +0530

    ADDED:Introduce CSVBuilderException class to ensure CSVBuilder encapsulated all changes

--- main/java/com/bridgelabz/service/StateCensusAnalyser.java
+++ main/java/com/bridgelabz/service/StateCensusAnalyser.java
@@ -1,10 +1,11 @@
 package com.bridgelabz.service;
 
 
-import com.bridgelabz.exception.StateCensusAnalyserException;
+import com.bridgelabz.exception.CSVBuilderException;
 import com.bridgelabz.model.CSVState;
 import com.bridgelabz.model.CSVStateCensus;
-import com.bridgelabz.utility.OpenCsvBuilder;
+import com.bridgelabz.utility.CSVBuilderFactory;
+import com.bridgelabz.utility.ICSVBuilder;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,53 +18,51 @@ import java.util.Iterator;
 public class StateCensusAnalyser {
     int countRecord = 0;
 
-    //Reading the data from csv file
-    public int loadCensusCsvData(String SAMPLE_CSV_PATH) throws StateCensusAnalyserException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_PATH));) {
-            Iterator<CSVStateCensus> censusCSVIterator = new OpenCsvBuilder().getCSVFileIterator(reader, CSVStateCensus.class);
+    //Reading and printing data from csv file
+    public int loadCensusCsvData(String SIMPLE_CSV_PATH) throws CSVBuilderException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(SIMPLE_CSV_PATH));) {
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            Iterator<CSVStateCensus> censusCSVIterator = csvBuilder.getCSVFileIterator(reader, CSVStateCensus.class);
             while (censusCSVIterator.hasNext()) {
                 CSVStateCensus csvStateCensus = censusCSVIterator.next();
                 countRecord++;
             }
             return countRecord;
         } catch (IOException e) {
-            throw new StateCensusAnalyserException
-                    (StateCensusAnalyserException.Exceptiontype.ENTERED_WRONG_FILE, e.getMessage());
+            throw new CSVBuilderException(CSVBuilderException.Exceptiontype.ENTERED_WRONG_FILE, e.getMessage());
         } catch (RuntimeException e) {
-            throw new StateCensusAnalyserException
-                    (StateCensusAnalyserException.Exceptiontype.ENTERED_INCORRECT_DELIMITER_OR_HEADER, e.getMessage());
+            throw new CSVBuilderException
+                    (CSVBuilderException.Exceptiontype.ENTERED_INCORRECT_DELIMITER_OR_HEADER, e.getMessage());
         }
     }
 
-    //Reading the data from statecode file
-    public int loadSateCodeCsvData(String CSV_PATH) throws StateCensusAnalyserException {
+    public int loadSateCodeCsvData(String CSV_PATH) throws CSVBuilderException {
         try (Reader reader = Files.newBufferedReader(Paths.get(CSV_PATH));) {
-            Iterator<CSVState> csvStatesCodeIterator = new OpenCsvBuilder()
-                    .getCSVFileIterator(reader, CSVState.class);
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            Iterator<CSVState> csvStateIterator = csvBuilder.getCSVFileIterator(reader, CSVState.class);
 
-            while (csvStatesCodeIterator.hasNext()) {
+            while (csvStateIterator.hasNext()) {
                 countRecord++;
-                CSVState csvStates = csvStatesCodeIterator.next();
+                CSVState csvState = csvStateIterator.next();
             }
             return countRecord;
         } catch (IOException e) {
-            throw new StateCensusAnalyserException
-                    (StateCensusAnalyserException.Exceptiontype.ENTERED_WRONG_FILE, e.getMessage());
+            throw new CSVBuilderException(CSVBuilderException.Exceptiontype.ENTERED_WRONG_FILE, e.getMessage());
         } catch (RuntimeException e) {
-            throw new StateCensusAnalyserException
-                    (StateCensusAnalyserException.Exceptiontype.ENTERED_INCORRECT_DELIMITER_OR_HEADER, e.getMessage());
+            throw new CSVBuilderException(CSVBuilderException.Exceptiontype.ENTERED_INCORRECT_DELIMITER_OR_HEADER, e.getMessage());
         }
     }
+
     //Read file extension
-    public static void getFileExtension(File file) throws StateCensusAnalyserException {
+    public static void getFileExtension(File file) throws CSVBuilderException {
         String extension = "";
         if (file != null) {
             String name = file.getName();
             extension = name.substring(name.lastIndexOf("."));
             if (!extension.equals(".csv")) {
-                throw new StateCensusAnalyserException
-                        (StateCensusAnalyserException.Exceptiontype.ENTERED_WRONG_FILE_TYPE, "FILE_TYPE_INCORRECT");
+                throw new CSVBuilderException(CSVBuilderException.Exceptiontype.ENTERED_WRONG_FILE_TYPE, "FILE_TYPE_INCORRECT");
             }
         }
     }
 }
+
