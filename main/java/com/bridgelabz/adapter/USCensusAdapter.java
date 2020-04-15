c692b3440694e20b53a9fd06354d61c515626fb2
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-15 19:56:21 +0530

    REFACTORED:Used DAO and DTO pattern to solve

---
+++ main/java/com/bridgelabz/adapter/USCensusAdapter.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.adapter;
+
+
+
+
+import com.bridgelabz.dao.CensusDAO;
+import com.bridgelabz.exception.CSVBuilderException;
+import com.bridgelabz.model.USCensus;
+
+import java.util.HashMap;
+
+public class USCensusAdapter extends CensusAdapter {
+    @Override
+    public HashMap<String, CensusDAO> loadCensusData(String... csvFilePath) throws CSVBuilderException {
+        return super.loadCensusData(USCensus.class, csvFilePath[0]);
+    }
+}
+
